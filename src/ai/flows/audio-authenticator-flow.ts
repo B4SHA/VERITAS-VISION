aae140e1afdfc9eeed48d1deda0123cb34877e3b
Skmd Sameer <sameerskmd085@gmail.com>
2025-10-03 17:47:22 +0000

    bro i already said 100s of times gemini-1.5-flash is depricated  it is n

--- src/ai/flows/audio-authenticator-flow.ts
+++ src/ai/flows/audio-authenticator-flow.ts
@@ -32,7 +32,7 @@ const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY || "");
 const jsonSchema = zodToJsonSchema(AudioAuthenticatorOutputSchema);
 
 const model = genAI.getGenerativeModel({
-    model: "gemini-1.5-flash",
+    model: "gemini-1.5-pro",
     generationConfig: {
         response_mime_type: "application/json",
         // @ts-ignore
